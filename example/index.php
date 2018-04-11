addfa73664bb5d8d6e7f46fd9f405bcdcb3ea5e2
Alexander Willam Vera Paco <averapaco@gmail.com>
2018-04-11 15:49:18 -0400

    first commit

---
+++ example/index.php
@@ -0,0 +1,7 @@
+<?php 
+
+include __DIR__ . "/../vendor/autoload.php";
+
+$hello = new \averapaco\hello\Hello();
+
+$hello->sayHello('composer');
---
+++ src/Hello.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace averapaco\hello;
+
+
+ /**
+ * class hollo example composer
+ */
+ class Hello
+ {
+ 	
+ 	public function sayHello($message='hola')
+ 	{
+ 		echo "Hello $message \n";
+ 	}
+ }
